4592b0e6dd377d3f803ec58f2f37d32506ce5374
Kyle Davies <kdavies311@gmail.com>
2019-01-30 13:45:42 -0800

    got table display on pge. able to set onlicks hardcoded withinHTML, going to refactor to have that func on our app.js

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,23 @@
+var test = function() {
+  document.getElementsByClassName('square').onclick = function(event) {
+  console.log(event);
+  }
+}
 
+//write our function that responds to a click
+  // will need to check whose turn it is and respond with either an X or a O in the desired square
+    // Will then need to run out below functions to see if there is a win, tie, or draw
+    // Will need to handle the case that you click on an already Occupied space.
+
+// Will need write a few functions
+  // check for winner. similar to our confilct code from nqueens, we can chec kt osee if we have 3 in a row.
+    // this check must run vertically, horizontally, and diagonally
+    //if win - alert and restart
+
+  //check if we have a draw
+    // what constitutes a draw. check spaces left?
+    // if draw - alert and restart
+
+
+// include a button on our page that allows us to refresh the table at any point in time.
 
