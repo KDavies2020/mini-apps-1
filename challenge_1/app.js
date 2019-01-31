edf1fdd3dce7696f14b66dca165d9791eeddf358
Kyle Davies <kdavies311@gmail.com>
2019-01-30 16:56:52 -0800

    X's and O's populating correctly. All tests for winning / tie games are functioning correctly. Need to add button / way to refresh page.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,26 +1,43 @@
-// general function to run when a square is clicked.
+// Counter to keep track of total turns.
 var counter = 0;
 
-var squares = document.getElementsByClassName("square")
+// Create a Board to Help us check for Conflicts.
+var board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 
-// SETTING UP OUR OVERALL ONLICK FUNCTION
-for(var i = 0; i < squares.length; i++) {
-  squares[i].onclick = function(event) { squareClicked()}
-}
-
-
-var squareClicked = function(square) {
-  console.log(event.target.className.split(', '))
-  var horizontalChecker = event.target.id.slice(0,1)
-  var verticalChecker = event.target.id.slice(1)
-  if (event.target.innerHTML === "") {
-    event.target.innerHTML = xORO();
+//If our board is full, game is a draw
+var countCheck = function() {
+  if (counter === 9) {
+    alert('Tie Game!')
   }
 };
 
-// var classesArray = {
-//   event.target.className.split(', ')
-// }
+//SETTING OUR ONCLICK FOR EACH SQUARE
+var squares = document.getElementsByClassName("board")
+
+for (var i = 0; i < squares.length; i++) {
+
+  squares[i].onclick = function(event) {
+    squareClicked();
+    countCheck();
+  }
+};
+
+
+//THE FUNCTION THAT IS ACTUALLY EXECUTED UPON A CLICK.
+
+var squareClicked = function(square) {
+
+  //creating the above array so we can loop and compare / check for winning combo
+  // var check = document.querySelector('.board').children;
+  // console.log(check);
+  if (event.target.innerHTML === "") {
+    event.target.innerHTML = xORO();
+    updateBoard()
+    findAWinner()
+
+  }
+};
+
 
 //FUNCTION TO CHECK WHETHER IT IS X OR O TURN
 
@@ -35,10 +52,66 @@ var xORO = function() {
   }
 };
 
+//UPDATE OUR BOARD TO REFLECT BOARD ON HTML PAGE
 
-//FUNCTION TO CHECK VERTICAL
+var updateBoard = function() {
+  var row = event.target.className;
+  var column = event.target.id;
+  board[row][column] = event.target.innerHTML;
+}
 
 
+//FUNCTION THAT CONTAINS OUR VERTICAL / HORIZONTAL / AND DIAG WINNING COMBOS
+
+var findAWinner = function() {
+  var row = event.target.className;
+  var column = event.target.id;
+  var horizontalCounter = '';
+  var verticalCounter = '';
+  var diagonalCounter = '';
+
+// INNER FUNCTION THAT CHECKS FOR A VERTICAL WIN
+
+  var verticalWinCheck = function() {
+
+    for (var i = 0; i <board.length; i++) {
+      verticalCounter += board[i][column];
+
+      if(verticalCounter === 'XXX'|| verticalCounter === 'OOO') {
+        alert("We Have A Winner!")
+       }
+    }
+
+  }
+
+  //INNER FUNCTION THAT CHECKS FOR A HORIZONTAL WIN
+
+  var horizontalWinCheck = function() {
+    for (var i = 0; i < board[row].length; i++) {
+      horizontalCounter += board[row][i];
+
+       if(horizontalCounter === 'XXX'|| horizontalCounter === 'OOO') {
+        alert("We Have A Winner!")
+       }
+
+    }
+  }
+
+  // DIAGONAL CHECKER - ONLY NEEDED TO RUN IF CORNERS ARE BEING USED UP
+
+  var diagonalCheck = function() {
+    if( board[0][0] + board[1][1] + board[2][2] === 'XXX' || board[0][0] + board[1][1] + board[2][2] === 'OOO') {
+      alert("We Have A Winner")
+    }
+    if( board[0][2] + board[1][1] + board[2][0] === 'XXX' || board[0][2] + board[1][1] + board[2][0] === 'OOO') {
+      alert("We Have A Winner")
+    }
+  }
+  horizontalWinCheck()
+  verticalWinCheck()
+  diagonalCheck()
+
+}
 //write our function that responds to a click
 // will need to check whose turn it is and respond with either an X or a O in the desired square
 // Will then need to run out below functions to see if there is a win, tie, or draw
