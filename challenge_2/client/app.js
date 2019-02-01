4faa31f97f3d20294dedd53059ccf6b55b13b8c4
Kyle Davies <kdavies311@gmail.com>
2019-01-31 17:14:44 -0800

    late commit - got index.html rendering, connected to server. am now getting text input to reach my server. Now just need to create our response function that will take data and turn it into csv structure.

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,17 @@
+// create a get request to the server that will load initial page.
 
+// create another get / possible post request for server on each form submission - will display the JSON data requested in cvs format.
+//  var clicked = function() {
+//   event.preventDefault();
+//   var files = document.querySelector('.submit').value;
+// console.log(files)
+//   fetch('http://127.0.0.1:3000/upload',
+//   {
+//     method: 'POST',
+//     body: files
+//   })
+//   .then(function(res){console.log(files)})
+//   .then(function(data) {alert(data)})
+
+// }
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,18 @@
 
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('client'))
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+ app.post('/upload', (req, res) => {
+
+  console.log('request body' + JSON.stringify(req.body))
+  res.send()
+
+ })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
