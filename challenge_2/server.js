7869c5aec1416bdf3d9c1dc7f87a057d25950740
Kyle Davies <kdavies311@gmail.com>
2019-01-31 20:50:57 -0800

    Well... totally broke my code unfortunately. Had my key names populating on to my page - went to create a func to populate the names in what csv style and broke everything :/

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -8,11 +8,13 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 
  app.post('/upload', (req, res) => {
-
-  console.log('request body' + JSON.stringify(req.body))
-  res.send()
+  output = []
+  for(var key in req.body.name) {
+    output.push(key)
+  }
+  console.log(JSON.parse(req.body.name))
+  res.send(Object.keys(req.body.name))
 
  })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
